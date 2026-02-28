8a0c858e31cd9438172aca74a5d00807acf85f82
Jayslen <jayslen134@gmail.com>
2026-02-28 17:24:17 -0400

    feat: implement method into the position struct. - Update position method - Create new position struct method

--- src/board.rs
+++ src/board.rs
@@ -1,14 +1,56 @@
 pub type Cell = Option<u8>;
 pub type Row = [Cell; 3];
 pub type Grid = [[Row; 3]; 3];
-
 pub type Board = [Grid; 3];
 
+const INDEX_LIMIT: u8 = 2;
+
 pub struct Position {
-    pub x: usize,
-    pub y: usize,
-    pub row: usize,
-    pub cell: usize,
+    pub x: u8,
+    pub y: u8,
+    pub row: u8,
+    pub cell: u8,
+}
+
+impl Position {
+    pub fn new() -> Position {
+        Position {
+            x: 0,
+            y: 0,
+            row: 0,
+            cell: 0,
+        }
+    }
+
+    pub fn update_position(&mut self) {
+        let Position { x, y, row, cell } = self;
+
+        println!("x: {}, y: {}, row: {}, cell: {}", x, y, row, cell);
+
+        //update cell and row
+        if *cell < INDEX_LIMIT {
+            *cell = Self::update_value(cell);
+        } else if *row < INDEX_LIMIT {
+            *cell = Self::update_value(cell);
+            *row = Self::update_value(row);
+        } else if *row == INDEX_LIMIT && *x < INDEX_LIMIT {
+            *x = Self::update_value(x);
+            *cell = Self::update_value(cell);
+            *row = Self::update_value(row);
+        } else {
+            *x = Self::update_value(x);
+            *y = Self::update_value(y);
+            *cell = Self::update_value(cell);
+            *row = Self::update_value(row);
+        }
+    }
+
+    fn update_value(value: &u8) -> u8 {
+        match value {
+            2 => 0,
+            _ => value + 1,
+        }
+    }
 }
 
 pub static GAME_BOARD: Board = [
--- src/values.rs
+++ src/values.rs
@@ -1,11 +1,11 @@
 use crate::board::{Board, Position};
 
 pub fn x_axis(board: &Board, position: &Position) -> Vec<u8> {
-    let current_y_grid = board[position.y];
+    let current_y_grid = board[position.y as usize];
     let mut values: Vec<u8> = Vec::new();
 
     for row in current_y_grid {
-        let row_values = row[position.x].iter();
+        let row_values = row[position.x as usize].iter();
         row_values.for_each(|cell| {
             if let Some(value) = cell {
                 values.push(*value);
@@ -19,9 +19,9 @@ pub fn x_axis(board: &Board, position: &Position) -> Vec<u8> {
 pub fn y_axis(board: &Board, position: &Position) -> Vec<u8> {
     let mut values: Vec<u8> = Vec::new();
     for col in board {
-        let row = col[position.x].iter();
+        let row = col[position.x as usize].iter();
         row.for_each(|cell| {
-            if let Some(value) = cell[position.cell] {
+            if let Some(value) = cell[position.cell as usize] {
                 values.push(value);
             }
         })
@@ -31,7 +31,7 @@ pub fn y_axis(board: &Board, position: &Position) -> Vec<u8> {
 
 pub fn current_grid(board: &Board, position: &Position) -> Vec<u8> {
     let mut values: Vec<u8> = Vec::new();
-    let current_grid = board[position.y][position.x];
+    let current_grid = board[position.y as usize][position.x as usize];
 
     for row in current_grid {
         row.iter().for_each(|cell| {
